1473db2eeb9f612e8c395c64b569cb479fdd0c80
Caspar William Bruenech <c.w.bruenech@pm.me>
2024-01-25 23:37:00 +0100

    started on formulae

--- src/RapidStellarEvolution.jl
+++ src/RapidStellarEvolution.jl
@@ -1,5 +1,15 @@
 module RapidStellarEvolution
 
+# struct RapidEvolutionStar
+#     mass
+#     age
+#     stellar_type
+#     metallicity
+#     a_n
+#     b_n
+
+# end
+
 include("fitting_coefficients.jl")
 
 end
